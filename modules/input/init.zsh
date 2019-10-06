b0a83a90d0c01fd5893b57688b860828a5809bcd
Ratheesh S <ratheeshreddy@gmail.com>
2019-10-06 11:12:57 +0530

    Move few functions from custom->input module
    Signed-off-by: Ratheesh S <ratheeshreddy@gmail.com>

--- modules/input/init.zsh
+++ modules/input/init.zsh
@@ -245,6 +245,75 @@ zle-line-finish() {
 }
 zle -N zle-line-finish
 
+# Other functions
+
+#this function tries to mimic vim's ctrl-a and ctrl-x bindings for
+# increasing or decreasing a number.
+function _increase_number() {
+  integer pos NUMBER i first last prelength diff
+  pos=$CURSOR
+  # find numbers starting from the left of the cursor to the end of the line
+  while [[ $BUFFER[$pos] != [[:digit:]] ]]; do
+    (( pos++ ))
+    (( $pos > $#BUFFER )) && return
+  done
+
+  # use the numeric argument and default to 1
+  # negate if called as decrease-number
+  NUMBER=${NUMERIC:-1}
+  if [[ $WIDGET = decrease-number ]]; then
+    (( NUMBER = 0 - $NUMBER ))
+  fi
+
+  # find the start of the number
+  i=$pos
+  while [[ $BUFFER[$i-1] = [[:digit:]] ]]; do
+    (( i-- ))
+  done
+  first=$i
+
+  # include one leading - if found
+  if [[ $BUFFER[$first-1] = - ]]; then
+    (( first-- ))
+  fi
+
+  # find the end of the number
+  i=$pos
+  while [[ $BUFFER[$i+1] = [[:digit:]] ]]; do
+    (( i++ ))
+  done
+  last=$i
+
+  # change the number and move cursor after it
+  prelength=$#BUFFER
+  (( BUFFER[$first,$last] += $NUMBER ))
+  (( diff = $#BUFFER - $prelength ))
+  (( CURSOR = last + diff ))
+}
+zle -N increase-number _increase_number
+zle -N decrease-number _increase_number
+
+# Use C-z to put current application to background and vice-versa
+fancy-ctrl-z () {
+    if [[ $#BUFFER -eq 0 ]]; then
+        BUFFER="fg"
+        zle accept-line
+    else
+        zle push-input
+        zle clear-screen
+    fi
+}
+zle -N fancy-ctrl-z
+
+# Inserts 'sudo ' at the beginning of the line.
+function prepend-sudo {
+    if [[ "$BUFFER" != su(do|)\ * ]]; then
+        BUFFER="sudo $BUFFER"
+        (( CURSOR += 5 ))
+    fi
+}
+zle -N prepend-sudo
+
 #
 # Emacs and Vi Key Bindings
 #
