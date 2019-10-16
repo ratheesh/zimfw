f98fdc86cfcf49fe350a3aa205471f8a28743594
Ratheesh S <ratheeshreddy@gmail.com>
2019-10-16 16:11:14 +0530

    Add forgit zsh module
    Module URL: https://github.com/wfxr/forgit.git
    
    PS: This module is added as a submodule
    
    Signed-off-by: Ratheesh S <ratheeshreddy@gmail.com>

---
+++ modules/forgit/init.zsh
@@ -0,0 +1,9 @@
+#
+# git wrapper with fzf
+#
+# Make git commands more visually pleasing and easier with the help of FZF
+#
+
+# source script
+source ${0:h}/external/forgit.plugin.zsh || return 1
+
