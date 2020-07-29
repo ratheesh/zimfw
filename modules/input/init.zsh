b9a881acc18c34859f2e12f50cdff79d35097dd5
Ratheesh S <ratheeshreddy@gmail.com>
2020-07-29 21:56:56 +0530

    input: reindent file

--- modules/input/init.zsh
+++ modules/input/init.zsh
@@ -17,37 +17,37 @@ WORDCHARS='*?_-.[]~&;!#$%^(){}<>'
 zmodload -F zsh/terminfo +b:echoti +p:terminfo
 typeset -gA key_info
 key_info=(
-  'Control'          '\C-'
-  'ControlLeft'      '\e[1;5D \e[5D \e\e[D \eOd'
-  'ControlRight'     '\e[1;5C \e[5C \e\e[C \eOc'
-  'ControlPageUp'    '\e[5;5~'
-  'ControlPageDown'  '\e[6;5~'
-  'Escape'           '\e'
-  'Meta'             '\M-'
-  'Backspace'        "^?"
-  'Delete'           "^[[3~"
-  'F1'               "$terminfo[kf1]"
-  'F2'               "$terminfo[kf2]"
-  'F3'               "$terminfo[kf3]"
-  'F4'               "$terminfo[kf4]"
-  'F5'               "$terminfo[kf5]"
-  'F6'               "$terminfo[kf6]"
-  'F7'               "$terminfo[kf7]"
-  'F8'               "$terminfo[kf8]"
-  'F9'               "$terminfo[kf9]"
-  'F10'              "$terminfo[kf10]"
-  'F11'              "$terminfo[kf11]"
-  'F12'              "$terminfo[kf12]"
-  'Insert'           "$terminfo[kich1]"
-  'Home'             "$terminfo[khome]"
-  'PageUp'           "$terminfo[kpp]"
-  'End'              "$terminfo[kend]"
-  'PageDown'         "$terminfo[knp]"
-  'Up'               "$terminfo[kcuu1]"
-  'Left'             "$terminfo[kcub1]"
-  'Down'             "$terminfo[kcud1]"
-  'Right'            "$terminfo[kcuf1]"
-  'BackTab'          "$terminfo[kcbt]"
+    'Control'          '\C-'
+    'ControlLeft'      '\e[1;5D \e[5D \e\e[D \eOd'
+    'ControlRight'     '\e[1;5C \e[5C \e\e[C \eOc'
+    'ControlPageUp'    '\e[5;5~'
+    'ControlPageDown'  '\e[6;5~'
+    'Escape'           '\e'
+    'Meta'             '\M-'
+    'Backspace'        "^?"
+    'Delete'           "^[[3~"
+    'F1'               "$terminfo[kf1]"
+    'F2'               "$terminfo[kf2]"
+    'F3'               "$terminfo[kf3]"
+    'F4'               "$terminfo[kf4]"
+    'F5'               "$terminfo[kf5]"
+    'F6'               "$terminfo[kf6]"
+    'F7'               "$terminfo[kf7]"
+    'F8'               "$terminfo[kf8]"
+    'F9'               "$terminfo[kf9]"
+    'F10'              "$terminfo[kf10]"
+    'F11'              "$terminfo[kf11]"
+    'F12'              "$terminfo[kf12]"
+    'Insert'           "$terminfo[kich1]"
+    'Home'             "$terminfo[khome]"
+    'PageUp'           "$terminfo[kpp]"
+    'End'              "$terminfo[kend]"
+    'PageDown'         "$terminfo[knp]"
+    'Up'               "$terminfo[kcuu1]"
+    'Left'             "$terminfo[kcub1]"
+    'Down'             "$terminfo[kcud1]"
+    'Right'            "$terminfo[kcuf1]"
+    'BackTab'          "$terminfo[kcbt]"
 )
 
 #
@@ -64,104 +64,104 @@ zle -N edit-command-line
 # Runs bindkey but for all of the keymaps. Running it with no arguments will
 # print out the mappings for all of the keymaps.
 function bindkey-all {
-  local keymap=''
-  for keymap in $(bindkey -l); do
-    [[ "$#" -eq 0 ]] && printf "#### %s\n" "${keymap}" 1>&2
-    bindkey -M "${keymap}" "$@"
-  done
+    local keymap=''
+    for keymap in $(bindkey -l); do
+        [[ "$#" -eq 0 ]] && printf "#### %s\n" "${keymap}" 1>&2
+        bindkey -M "${keymap}" "$@"
+    done
 }
 
 # Exposes information about the Zsh Line Editor via the $editor_info associative
 # array.
 function editor-info {
-  # Clean up previous $editor_info.
-  unset editor_info
-  typeset -gA editor_info
+    # Clean up previous $editor_info.
+    unset editor_info
+    typeset -gA editor_info
 
-  if [[ "$KEYMAP" = 'vicmd' ]]; then
-      zstyle -s ':zim:input:info:keymap:alternate' format 'REPLY'
-      editor_info[keymap]="$REPLY"
-      editor_info[mode]="       %F{60}---%B%F{208}NORMAL%F{60}%b---%f"
-  elif [[ "$KEYMAP" = 'viins' || "$KEYMAP" = 'main' ]];then
-      zstyle -s ':zim:input:info:keymap:primary' format 'REPLY'
-      editor_info[mode]="       %F{60}---%B%F{33}INSERT%F{60}%b---%f"
-      editor_info[keymap]="$REPLY"
+    if [[ "$KEYMAP" = 'vicmd' ]]; then
+        zstyle -s ':zim:input:info:keymap:alternate' format 'REPLY'
+        editor_info[keymap]="$REPLY"
+        editor_info[mode]="       %F{60}---%B%F{208}NORMAL%F{60}%b---%f"
+    elif [[ "$KEYMAP" = 'viins' || "$KEYMAP" = 'main' ]];then
+        zstyle -s ':zim:input:info:keymap:primary' format 'REPLY'
+        editor_info[mode]="       %F{60}---%B%F{33}INSERT%F{60}%b---%f"
+        editor_info[keymap]="$REPLY"
 
-      if [[ "$ZLE_STATE" == *overwrite* ]]; then
-          zstyle -s ':zim:input:info:keymap:primary:overwrite' format 'REPLY'
-          editor_info[overwrite]="$REPLY"
-      else
-          zstyle -s ':zim:input:info:keymap:primary:insert' format 'REPLY'
-          editor_info[overwrite]="$REPLY"
-      fi
-  elif [[ "$KEYMAP" = 'vivis' || "$KEYMAP" = 'vivli' ]];then
-      zstyle -s ':zim:input:info:keymap:alternate' format 'REPLY'
-      editor_info[keymap]="$REPLY"
-      editor_info[mode]="       %F{60}---%B%F{5}VISUAL%F{60}%b---%f"
-  fi
+        if [[ "$ZLE_STATE" == *overwrite* ]]; then
+            zstyle -s ':zim:input:info:keymap:primary:overwrite' format 'REPLY'
+            editor_info[overwrite]="$REPLY"
+        else
+            zstyle -s ':zim:input:info:keymap:primary:insert' format 'REPLY'
+            editor_info[overwrite]="$REPLY"
+        fi
+    elif [[ "$KEYMAP" = 'vivis' || "$KEYMAP" = 'vivli' ]];then
+        zstyle -s ':zim:input:info:keymap:alternate' format 'REPLY'
+        editor_info[keymap]="$REPLY"
+        editor_info[mode]="       %F{60}---%B%F{5}VISUAL%F{60}%b---%f"
+    fi
 
-  unset REPLY
-  zle zle-reset-prompt
+    unset REPLY
+    zle zle-reset-prompt
 }
 zle -N editor-info
 
 # Reset the prompt based on the current context and
 # the ps-context option.
 function zle-reset-prompt {
-  if zstyle -t ':prezto:input' ps-context; then
-    # If we arent within one of the specified contexts, then we want to reset
-    # the prompt with the appropriate editor_info[keymap] if there is one.
-    if [[ $CONTEXT != (select|cont) ]]; then
-      zle .reset-prompt
-      zle -R
-    fi
-  else
-    zle .reset-prompt
-    zle -R
-  fi
-}
+    if zstyle -t ':prezto:input' ps-context; then
+        # If we arent within one of the specified contexts, then we want to reset
+        # the prompt with the appropriate editor_info[keymap] if there is one.
+        if [[ $CONTEXT != (select|cont) ]]; then
+            zle .reset-prompt
+            zle -R
+        fi
+    else
+        zle .reset-prompt
+        zle -R
+        fi
+    }
 zle -N zle-reset-prompt
 
 # Updates editor information when the keymap changes.
 function zle-keymap-select {
-  zle editor-info
+    zle editor-info
 }
 zle -N zle-keymap-select
 
 # Toggles emacs overwrite mode and updates editor information.
 function overwrite-mode {
-  zle .overwrite-mode
-  zle editor-info
+    zle .overwrite-mode
+    zle editor-info
 }
 zle -N overwrite-mode
 
 # Enters vi insert mode and updates editor information.
 function vi-insert {
-  zle .vi-insert
-  zle editor-info
+    zle .vi-insert
+    zle editor-info
 }
 zle -N vi-insert
 
 # Moves to the first non-blank character then enters vi insert mode and updates
 # editor information.
 function vi-insert-bol {
-  zle .vi-insert-bol
-  zle editor-info
+    zle .vi-insert-bol
+    zle editor-info
 }
 zle -N vi-insert-bol
 
 # Enters vi replace mode and updates editor information.
 function vi-replace  {
-  zle .vi-replace
-  zle editor-info
+    zle .vi-replace
+    zle editor-info
 }
 zle -N vi-replace
 
 # Expand aliases
 function glob-alias {
-  zle _expand_alias
-  zle expand-word
-  zle magic-space
+    zle _expand_alias
+    zle expand-word
+    zle magic-space
 }
 zle -N glob-alias
 
@@ -171,46 +171,46 @@ zle -N glob-alias
 # This is currently only used for the emacs keys because vi-pound-insert has
 # been reported to work properly.
 function pound-toggle {
-  if [[ "$BUFFER" = '#'* ]]; then
-    # Because of an oddity in how zsh handles the cursor when the buffer size
-    # changes, we need to make this check before we modify the buffer and let
-    # zsh handle moving the cursor back if its past the end of the line.
-    if [[ $CURSOR != $#BUFFER ]]; then
-      (( CURSOR -= 1 ))
-    fi
-    BUFFER="${BUFFER:1}"
-  else
-    BUFFER="#$BUFFER"
-    (( CURSOR += 1 ))
-  fi
-}
+    if [[ "$BUFFER" = '#'* ]]; then
+        # Because of an oddity in how zsh handles the cursor when the buffer size
+        # changes, we need to make this check before we modify the buffer and let
+        # zsh handle moving the cursor back if its past the end of the line.
+        if [[ $CURSOR != $#BUFFER ]]; then
+            (( CURSOR -= 1 ))
+        fi
+        BUFFER="${BUFFER:1}"
+    else
+        BUFFER="#$BUFFER"
+        (( CURSOR += 1 ))
+        fi
+    }
 zle -N pound-toggle
 
 # Bind the keys
 
 # Expands .... to ../..
 if [[ ${zdouble_dot_expand} == 'true' ]]; then
-  double-dot-expand() {
+    double-dot-expand() {
     if [[ ${LBUFFER} == *.. ]]; then
-      LBUFFER+='/..'
+        LBUFFER+='/..'
     else
-      LBUFFER+='.'
+        LBUFFER+='.'
     fi
-  }
-  zle -N double-dot-expand
+}
+zle -N double-dot-expand
 fi
 
 # Displays an indicator when completing.
 function expand-or-complete-with-indicator {
-  local indicator
-  zstyle -s ':zim:input:info:completing' format 'indicator'
+    local indicator
+    zstyle -s ':zim:input:info:completing' format 'indicator'
 
   # This is included to work around a bug in zsh which shows up when interacting
   # with multi-line prompts.
   if [[ -z "$indicator" ]]; then
-    sleep .1
-    zle expand-or-complete
-    return
+      sleep .1
+      zle expand-or-complete
+      return
   fi
 
   [[ -n "$terminfo[rmam]" && -n "$terminfo[smam]" ]] && echoti rmam
@@ -235,18 +235,18 @@ zle -N expand-or-complete-with-indicator
 
 # Put into application mode and validate ${terminfo}
 zle-line-init() {
-  if (( ${+terminfo[smkx]} )); then
+if (( ${+terminfo[smkx]} )); then
     echoti smkx
-  fi
-  zle editor-info
+fi
+zle editor-info
 }
 zle -N zle-line-init
 
 zle-line-finish() {
-  if (( ${+terminfo[rmkx]} )); then
+if (( ${+terminfo[rmkx]} )); then
     echoti rmkx
-  fi
-  zle editor-info
+fi
+zle editor-info
 }
 zle -N zle-line-finish
 
@@ -255,37 +255,37 @@ zle -N zle-line-finish
 #this function tries to mimic vim's ctrl-a and ctrl-x bindings for
 # increasing or decreasing a number.
 function _increase_number() {
-  integer pos NUMBER i first last prelength diff
-  pos=$CURSOR
-  # find numbers starting from the left of the cursor to the end of the line
-  while [[ $BUFFER[$pos] != [[:digit:]] ]]; do
-    (( pos++ ))
-    (( $pos > $#BUFFER )) && return
-  done
+    integer pos NUMBER i first last prelength diff
+    pos=$CURSOR
+    # find numbers starting from the left of the cursor to the end of the line
+    while [[ $BUFFER[$pos] != [[:digit:]] ]]; do
+        (( pos++ ))
+        (( $pos > $#BUFFER )) && return
+    done
 
   # use the numeric argument and default to 1
   # negate if called as decrease-number
   NUMBER=${NUMERIC:-1}
   if [[ $WIDGET = decrease-number ]]; then
-    (( NUMBER = 0 - $NUMBER ))
+      (( NUMBER = 0 - $NUMBER ))
   fi
 
   # find the start of the number
   i=$pos
   while [[ $BUFFER[$i-1] = [[:digit:]] ]]; do
-    (( i-- ))
+      (( i-- ))
   done
   first=$i
 
   # include one leading - if found
   if [[ $BUFFER[$first-1] = - ]]; then
-    (( first-- ))
+      (( first-- ))
   fi
 
   # find the end of the number
   i=$pos
   while [[ $BUFFER[$i+1] = [[:digit:]] ]]; do
-    (( i++ ))
+      (( i++ ))
   done
   last=$i
 
@@ -300,13 +300,13 @@ zle -N decrease-number _increase_number
 
 # Use C-z to put current application to background and vice-versa
 fancy-ctrl-z () {
-    if [[ $#BUFFER -eq 0 ]]; then
-        BUFFER="fg"
-        zle accept-line
-    else
-        zle push-input
-        zle clear-screen
-    fi
+if [[ $#BUFFER -eq 0 ]]; then
+    BUFFER="fg"
+    zle accept-line
+else
+    zle push-input
+    zle clear-screen
+fi
 }
 zle -N fancy-ctrl-z
 
@@ -346,33 +346,33 @@ function _zim-zle-noop {  ; }
 zle -N _zim-zle-noop
 local -a unbound_keys
 unbound_keys=(
-  "${key_info[F1]}"
-  "${key_info[F2]}"
-  "${key_info[F3]}"
-  "${key_info[F4]}"
-  "${key_info[F5]}"
-  "${key_info[F6]}"
-  "${key_info[F7]}"
-  "${key_info[F8]}"
-  "${key_info[F9]}"
-  "${key_info[F10]}"
-  "${key_info[F11]}"
-  "${key_info[F12]}"
-  "${key_info[PageUp]}"
-  "${key_info[PageDown]}"
-  "${key_info[ControlPageUp]}"
-  "${key_info[ControlPageDown]}"
+    "${key_info[F1]}"
+    "${key_info[F2]}"
+    "${key_info[F3]}"
+    "${key_info[F4]}"
+    "${key_info[F5]}"
+    "${key_info[F6]}"
+    "${key_info[F7]}"
+    "${key_info[F8]}"
+    "${key_info[F9]}"
+    "${key_info[F10]}"
+    "${key_info[F11]}"
+    "${key_info[F12]}"
+    "${key_info[PageUp]}"
+    "${key_info[PageDown]}"
+    "${key_info[ControlPageUp]}"
+    "${key_info[ControlPageDown]}"
 )
 
 for keymap in $unbound_keys; do
-  bindkey -M viins "${keymap}" _zim-zle-noop
-  bindkey -M vicmd "${keymap}" _zim-zle-noop
+    bindkey -M viins "${keymap}" _zim-zle-noop
+    bindkey -M vicmd "${keymap}" _zim-zle-noop
 done
 
 # Keybinds for all keymaps
 for keymap in 'emacs' 'viins' 'vicmd'; do
-  bindkey -M "$keymap" "$key_info[Home]" beginning-of-line
-  bindkey -M "$keymap" "$key_info[End]" end-of-line
+    bindkey -M "$keymap" "$key_info[Home]" beginning-of-line
+    bindkey -M "$keymap" "$key_info[End]" end-of-line
 done
 
 # Keybinds for all vi keymaps
@@ -493,9 +493,9 @@ if [[ "$key_bindings" == vi ]]; then
     bindkey -M vicmd ds delete-surround
     bindkey -M vicmd ys add-surround
     bindkey -M visual S add-surround
-fi
+    fi
 
-unset key{,map,_bindings}
+    unset key{,map,_bindings}
 
 #  vim: set ft=zsh ff=unix ts=8 sw=4 tw=0 expandtab:
 # End of File
